cdaba6228137e39695f1e3f2746c81132bb35372
kmochocki <krzysztof.mochocki@gmail.com>
2024-12-03 18:07:44 +0100

    Solve day1

--- day1/src/main/kotlin/Day1.kt
+++ day1/src/main/kotlin/Day1.kt
@@ -1,16 +1,42 @@
+import kotlin.math.absoluteValue
+
 fun main() {
     println(part1())
     println(part2())
 }
 
 fun part1(): Any {
-    return input
-
+    val (firstList, secondList) = inputAsLists()
+    val firstSorted = firstList.asSequence()
+        .map { it.value }
+        .sorted()
+    val secondSorted = secondList.asSequence()
+        .map { it.value }
+        .sorted()
+    val result = firstSorted.zip(secondSorted) { first, second -> first - second }
+        .map { it.absoluteValue }
+        .sum()
+    return result
 }
 
 fun part2(): Any {
-    return input.reversed()
+    val (firstList, secondList) = inputAsLists()
+    val counts = secondList.asSequence()
+        .map { it.value }
+        .groupingBy { it }
+        .eachCount()
+    return firstList.asSequence()
+        .map { it.value }
+        .map { counts[it]?.times(it) ?: 0 }
+        .sum()
 }
+
+private fun inputAsLists() = input.splitToSequence("\n")
+    .filter { it.isNotBlank() }
+    .flatMap { it.splitToSequence("   ") }
+    .map { it.toInt() }
+    .withIndex()
+    .partition { it.index % 2 == 0 }
 
 private val input: String by lazy {readInput()}
 

033d209cdeb7f1db7d7856095dc183011f52d4da
kmochocki <krzysztof.mochocki@gmail.com>
2024-12-05 17:30:54 +0100

    Solve day2

--- day2/src/main/kotlin/Day2.kt
+++ day2/src/main/kotlin/Day2.kt
@@ -4,12 +4,41 @@ fun main() {
 }
 
 fun part1(): Any {
-    return input
-
+    return input.splitToSequence("\n")
+        .filter { record -> record.isNotBlank() }
+        .map { record ->
+            val differences = record.splitToSequence(" ")
+                .map { it.toInt() }
+                .zipWithNext()
+                .map { it.first - it.second }
+            differences.all { it in 1..3 } or differences.all { it in -3..-1 }
+        }
+        .filter { it }
+        .count()
 }
 
 fun part2(): Any {
-    return input.reversed()
+    return input.splitToSequence("\n")
+        .filter { record -> record.isNotBlank() }
+        .map { record ->
+            val report = record.splitToSequence(" ")
+                .map { it.toInt() }
+                .withIndex()
+            var isSafe = false
+            for (i in 0 until report.count()) {
+                val differences = report.filter { it.index != i }
+                    .map { it.value }
+                    .zipWithNext()
+                    .map { it.first - it.second }
+                isSafe = differences.all { it in 1..3 } or differences.all { it in -3..-1 }
+                if (isSafe) {
+                    break
+                }
+            }
+            isSafe
+        }
+        .filter { it }
+        .count()
 }
 
 private val input: String by lazy {readInput()}

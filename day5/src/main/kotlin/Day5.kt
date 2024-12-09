d270a3286594be9d2413edb955926c8cb0c02fd8
kmochocki <krzysztof.mochocki@gmail.com>
2024-12-09 18:12:56 +0100

    Solve day5

--- day5/src/main/kotlin/Day5.kt
+++ day5/src/main/kotlin/Day5.kt
@@ -4,12 +4,71 @@ fun main() {
 }
 
 fun part1(): Any {
-    return input
-
+    val splitted = input.split("\\n\\n".toRegex())
+    val rules = splitted[0].splitToSequence("\n")
+        .map { it.split("|") }
+        .map { it[0].toInt() to it[1].toInt() }
+        .groupBy(Pair<Int, Int>::first) { it.second }
+    return splitted[1].splitToSequence("\n")
+        .filter { it.isNotBlank() }
+        .map { updates ->
+            updates.splitToSequence(",")
+                .map { it.toInt() }
+                .toList()
+        }
+        .filter { updates ->
+            var isValid = true
+            for (i in updates.size - 1 downTo 1) {
+                val precedingUpdates = updates.take(i)
+                isValid = rules[updates[i]]?.none { precedingUpdates.contains(it) } ?: false
+                if (isValid.not()) {
+                    break
+                }
+            }
+            isValid
+        }
+        .map { updates -> updates[updates.size / 2] }
+        .sum()
 }
 
 fun part2(): Any {
-    return input.reversed()
+    val splitted = input.split("\\n\\n".toRegex())
+    val rules = splitted[0].splitToSequence("\n")
+        .map { it.split("|") }
+        .map { it[0].toInt() to it[1].toInt() }
+        .groupBy(Pair<Int, Int>::first) { it.second }
+    return splitted[1].splitToSequence("\n")
+        .filter { it.isNotBlank() }
+        .map { updates ->
+            updates.splitToSequence(",")
+                .map { it.toInt() }
+                .toList()
+        }
+        .filter { updates ->
+            var isValid = true
+            for (i in updates.size - 1 downTo 1) {
+                val precedingUpdates = updates.take(i)
+                isValid = rules[updates[i]]?.none { precedingUpdates.contains(it) } ?: true
+                if (!isValid) {
+                    break
+                }
+            }
+            !isValid
+        }
+        .map {
+            it.sortedWith(Comparator { o1, o2 ->
+                val result: Int
+                val ints = rules[o1] ?: listOf()
+                result = if (ints.contains(o2)) {
+                    -1
+                } else {
+                    1
+                }
+                result
+            })
+        }
+        .map { updates -> updates[updates.size / 2] }
+        .sum()
 }
 
 private val input: String by lazy {readInput()}

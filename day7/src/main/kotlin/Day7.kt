eae7079dbfa914faec52359d908983dd1406800c
kmochocki <krzysztof.mochocki@gmail.com>
2024-12-12 20:06:02 +0100

    Solve day7

--- day7/src/main/kotlin/Day7.kt
+++ day7/src/main/kotlin/Day7.kt
@@ -3,13 +3,77 @@ fun main() {
     println(part2())
 }
 
-fun part1(): Any {
-    return input
+val operationsPart1: List<(Long, Long) -> Long> = listOf(
+    { a: Long, b: Long -> a + b },
+    { a: Long, b: Long -> a * b },
+)
 
+fun part1(): Any {
+    return input.splitToSequence("\n")
+        .filter { it.isNotBlank() }
+        .mapNotNull {
+            val (testValueString, numbersString) = it.split(":")
+            val test = testValueString.toLong()
+            val numbers = numbersString.split(" ")
+                .filter(String::isNotBlank)
+                .map(String::toLong)
+            if (calculatePart1(numbers).any { result -> result == test }) {
+                test
+            } else {
+                null
+            }
+        }
+        .sum()
 }
 
+private fun calculatePart1(rest: List<Long>): List<Long> {
+    return if (rest.size > 1) {
+        calculatePart1(rest.take(rest.size - 1))
+            .flatMap { result ->
+                operationsPart1.map { operation ->
+                    operation(result, rest.last())
+                }
+            }
+    } else {
+        listOf(rest.first())
+    }
+}
+
+val operationsPart2: List<(Long, Long) -> Long> = listOf(
+    { a: Long, b: Long -> a + b },
+    { a: Long, b: Long -> a * b },
+    { a: Long, b: Long -> (a.toString() + b.toString()).toLong() },
+)
+
 fun part2(): Any {
-    return input.reversed()
+    return input.splitToSequence("\n")
+        .filter { it.isNotBlank() }
+        .mapNotNull {
+            val (testValueString, numbersString) = it.split(":")
+            val test = testValueString.toLong()
+            val numbers = numbersString.split(" ")
+                .filter(String::isNotBlank)
+                .map(String::toLong)
+            if (calculatePart2(numbers).any { result -> result == test }) {
+                test
+            } else {
+                null
+            }
+        }
+        .sum()
+}
+
+private fun calculatePart2(rest: List<Long>): List<Long> {
+    return if (rest.size > 1) {
+        calculatePart2(rest.take(rest.size - 1))
+            .flatMap { result ->
+                operationsPart2.map { operation ->
+                    operation(result, rest.last())
+                }
+            }
+    } else {
+        listOf(rest.first())
+    }
 }
 
 private val input: String by lazy {readInput()}

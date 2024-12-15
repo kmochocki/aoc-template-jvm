e5da2b77ea07499144ecfb328cca18b08f07418d
kmochocki <krzysztof.mochocki@gmail.com>
2024-12-15 11:37:02 +0100

    Optimize day7

--- day7/src/main/kotlin/Day7.kt
+++ day7/src/main/kotlin/Day7.kt
@@ -9,34 +9,38 @@ val operationsPart1: List<(Long, Long) -> Long> = listOf(
 )
 
 fun part1(): Any {
-    return input.splitToSequence("\n")
+    return input.lineSequence()
         .filter { it.isNotBlank() }
         .mapNotNull {
             val (testValueString, numbersString) = it.split(":")
             val test = testValueString.toLong()
-            val numbers = numbersString.split(" ")
+            val numbers = numbersString.splitToSequence(" ")
                 .filter(String::isNotBlank)
                 .map(String::toLong)
-            if (calculatePart1(numbers).any { result -> result == test }) {
-                test
-            } else {
-                null
-            }
+                .toList()
+            if (calculatePart1(numbers, test)) test else null
         }
         .sum()
 }
 
-private fun calculatePart1(rest: List<Long>): List<Long> {
-    return if (rest.size > 1) {
-        calculatePart1(rest.take(rest.size - 1))
-            .flatMap { result ->
-                operationsPart1.map { operation ->
-                    operation(result, rest.last())
-                }
-            }
-    } else {
-        listOf(rest.first())
+private fun calculatePart1(rest: List<Long>, target: Long): Boolean {
+    return calculatePart1Inner(rest, target, 1, rest.first())
+}
+
+fun calculatePart1Inner(
+    rest: List<Long>,
+    target: Long,
+    index: Int,
+    current: Long
+): Boolean {
+    if (index == rest.size) return current == target
+
+    val next = rest[index]
+    for (operation in operationsPart1) {
+        if (calculatePart1Inner(rest, target, index + 1, operation(current, next))) return true
     }
+
+    return false
 }
 
 val operationsPart2: List<(Long, Long) -> Long> = listOf(
@@ -46,34 +50,38 @@ val operationsPart2: List<(Long, Long) -> Long> = listOf(
 )
 
 fun part2(): Any {
-    return input.splitToSequence("\n")
+    return input.lineSequence()
         .filter { it.isNotBlank() }
         .mapNotNull {
             val (testValueString, numbersString) = it.split(":")
             val test = testValueString.toLong()
-            val numbers = numbersString.split(" ")
+            val numbers = numbersString.splitToSequence(" ")
                 .filter(String::isNotBlank)
                 .map(String::toLong)
-            if (calculatePart2(numbers).any { result -> result == test }) {
-                test
-            } else {
-                null
-            }
+                .toList()
+            if (calculatePart2(numbers, test)) test else null
         }
         .sum()
 }
 
-private fun calculatePart2(rest: List<Long>): List<Long> {
-    return if (rest.size > 1) {
-        calculatePart2(rest.take(rest.size - 1))
-            .flatMap { result ->
-                operationsPart2.map { operation ->
-                    operation(result, rest.last())
-                }
-            }
-    } else {
-        listOf(rest.first())
+private fun calculatePart2(rest: List<Long>, target: Long): Boolean {
+    return calculatePart2Inner(rest, target, 1, rest.first())
+}
+
+fun calculatePart2Inner(
+    rest: List<Long>,
+    target: Long,
+    index: Int,
+    current: Long
+): Boolean {
+    if (index == rest.size) return current == target
+
+    val next = rest[index]
+    for (operation in operationsPart2) {
+        if (calculatePart2Inner(rest, target, index + 1, operation(current, next))) return true
     }
+
+    return false
 }
 
 private val input: String by lazy {readInput()}
